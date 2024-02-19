d0badae8865d43739305bca13a640f22a50febb0
ricardom98 <r.moralesbus@gmail.com>
2024-02-18 19:03:25 -0600

    pep8 test_reservation

--- A01740032_A62/tests/test_reservation.py
+++ A01740032_A62/tests/test_reservation.py
@@ -1,4 +1,5 @@
-# test_reservation.py
+'''Class for testing reservation module'''
+
 import unittest
 from unittest.mock import patch
 from io import StringIO
@@ -6,17 +7,20 @@ from A01740032_A62.classes.hotel import Hotel
 from A01740032_A62.classes.customer import Customer
 from A01740032_A62.classes.reservation import Reservation
 
+
 class TestReservation(unittest.TestCase):
-    
+    '''Class for testing reservation module'''
+
     def setUp(self):
         """Este método se ejecuta antes de cada prueba"""
-        self.customer = Customer("C001", "Juan Pérez", "juan.perez@example.com")
+        self.customer = Customer("C001", "Juan Pérez", "juanperez@example.com")
         self.hotel = Hotel(1, "Hotel Prueba", "Ciudad Prueba", 100)
-        self.reservation = Reservation("R001", self.customer, self.hotel, "2023-01-01", "2023-01-10")
+        self.reservation = Reservation("R01", self.customer,
+                                       self.hotel, "2023-01-01", "2023-01-10")
 
     def test_creacion_reservation(self):
         """Test para verificar la correcta creación de un objeto Reservation"""
-        self.assertEqual(self.reservation.reservation_id, "R001")
+        self.assertEqual(self.reservation.reservation_id, "R01")
         self.assertEqual(self.reservation.customer, self.customer)
         self.assertEqual(self.reservation.hotel, self.hotel)
         self.assertEqual(self.reservation.start_date, "2023-01-01")
@@ -30,18 +34,19 @@ class TestReservation(unittest.TestCase):
         self.assertEqual(self.reservation.get_status(), nuevo_estado)
 
     def test_mostrar_informacion(self):
-        """Test para verificar que mostrar_informacion imprime la salida correcta."""
+        """Test para verificar que mostrar_informacion es correcta."""
         expected_output = (
-            f"ID de la Reservación: R001\n"
-            f"ID del Cliente: C001\n"
-            f"ID del Hotel: 1\n"
-            f"Fecha de Inicio: 2023-01-01\n"
-            f"Fecha de Fin: 2023-01-10\n"
-            f"Estado: activa\n"
+            "ID de la Reservación: R01\n"
+            "ID del Cliente: C001\n"
+            "ID del Hotel: 1\n"
+            "Fecha de Inicio: 2023-01-01\n"
+            "Fecha de Fin: 2023-01-10\n"
+            "Estado: activa\n"
         )
         with patch('sys.stdout', new=StringIO()) as fake_out:
             self.reservation.mostrar_informacion()
             self.assertEqual(fake_out.getvalue(), expected_output)
+
 
 if __name__ == '__main__':
     unittest.main()
